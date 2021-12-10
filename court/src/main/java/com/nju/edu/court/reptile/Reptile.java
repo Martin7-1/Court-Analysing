5c23e2db86bc72a17785fb550f8223b181ed653b
Martin77 <1754350460@qq.com>
2021-12-10 11:48:05 +0800

    update reptile

--- court/src/main/java/com/nju/edu/court/reptile/Reptile.java
+++ court/src/main/java/com/nju/edu/court/reptile/Reptile.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.ArrayList;
@@ -28,15 +29,24 @@ public class Reptile {
         System.setProperty("webdriver.chrome.driver", driverPath);
         System.out.println("webdriver path: " + driverPath);
         driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         wait = new WebDriverWait(driver, 10);
 
         String url = "https://wenshu.court.gov.cn/";
         try {
             driver.get(url);
-            // 最大化页面
-            TimeUnit.SECONDS.sleep(2);
             // 登录
             login();
+            // 点击刑事案件
+            // WebElement criminalCase = driver.findElement(By.xpath("/html/body/div[1]/div[3]/div[2]/diy:lawyee/div/ul/li[2]/a"));
+            // clickButton(criminalCase);
+
+            // 搜索栏
+            WebElement search = driver.findElement(By.xpath("/html/body/div[1]/div[3]/div[3]/diy:lawyee/div/div[1]/div[2]/input"));
+            search.clear();
+            search.sendKeys("抢劫");
+            WebElement searchButton = driver.findElement(By.xpath("/html/body/div[1]/div[3]/div[3]/diy:lawyee/div/div[1]/div[2]/input"));
+            clickButton(searchButton);
         } catch (InterruptedException e) {
             System.err.println("sleep interrupted!");
         } finally {
@@ -44,42 +54,32 @@ public class Reptile {
         }
     }
 
+    private void clickButton(WebElement button) throws InterruptedException {
+        button.click();
+    }
+
     private void login() throws InterruptedException {
         // 登录界面
         WebElement login = driver.findElement(By.xpath("/html/body/div[1]/div[2]/ul/li[1]/a"));
-        // 防止过快点击
-        TimeUnit.SECONDS.sleep(10);
         System.out.println(login.getText());
         // 点击登录界面
         login.click();
-        // wait until first element is visible
-//        wait.until(ExpectedConditions.elementToBeSelected(By.xpath("/html/body/div/h3/img")));
 
         // 刷新
-        TimeUnit.SECONDS.sleep(2);
         driver.navigate().refresh();
-        // 刷新
-        TimeUnit.SECONDS.sleep(10);
         driver.navigate().refresh();
-        TimeUnit.SECONDS.sleep(20);
         // 登录界面在一个iframe上，需要切换到iframe
         WebElement iframe = driver.findElement(By.tagName("iframe"));
         driver.switchTo().frame(iframe);
-        // TODO: 输入账号和密码
         WebElement userName = driver.findElement(By.xpath("/html/body/div[2]/div/form/div/div[1]/div/div/div/input"));
         // 首先清空输入栏
         userName.clear();
         userName.sendKeys("15859579166");
-        // 防止过快点击ip被封
-        TimeUnit.SECONDS.sleep(10);
         WebElement password = driver.findElement(By.xpath("/html/body/div[2]/div/form/div/div[2]/div/div/div/input"));
         password.clear();
         password.sendKeys("Zzzyi123456");
-        // 防止过快点击ip被封
-        TimeUnit.SECONDS.sleep(10);
         WebElement button = driver.findElement(By.xpath("/html/body/div[2]/div/form/div/div[3]/span"));
         button.click();
-        TimeUnit.SECONDS.sleep(10);
     }
 
     private List<String> initOptions() {
