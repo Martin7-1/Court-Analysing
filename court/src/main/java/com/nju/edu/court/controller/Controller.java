986664ddfa521650016283fa218cd4949f00a4d8
Martin77 <1754350460@qq.com>
2022-01-15 15:06:12 +0800

    update nlp

--- court/src/main/java/com/nju/edu/court/controller/Controller.java
+++ court/src/main/java/com/nju/edu/court/controller/Controller.java
@@ -1,21 +1,23 @@
 package com.nju.edu.court.controller;
 
+import com.nju.edu.court.entity.Analysis;
 import edu.stanford.nlp.ling.CoreAnnotations;
 import edu.stanford.nlp.ling.CoreLabel;
 import edu.stanford.nlp.pipeline.Annotation;
 import edu.stanford.nlp.pipeline.StanfordCoreNLP;
-import edu.stanford.nlp.semgraph.SemanticGraph;
-import edu.stanford.nlp.semgraph.SemanticGraphCoreAnnotations;
 import edu.stanford.nlp.trees.Tree;
 import edu.stanford.nlp.trees.TreeCoreAnnotations;
 import edu.stanford.nlp.util.CoreMap;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author Zyi
@@ -23,73 +25,23 @@ import java.util.List;
 @RestController
 public class Controller {
 
-    StanfordCoreNLP pipeline;
+    private Analysis analysis;
 
-    @GetMapping("/nlp")
-    public String getStr(@RequestParam(value = "text", defaultValue = "我是一名大学生") String text) {
-        init();
-        return segInCh(text);
+    /**
+     * 返回词性分析的结果
+     * @return 词性分析结果，词性 - 对应的单词
+     */
+    @GetMapping("/getResult")
+    public Map<String, List<String>> sendMessage() {
+        return null;
     }
 
-    public List<String> get(String text) {
-        String newText = segInCh(text);
-        return new ArrayList<>(Arrays.asList(newText.split("\n")));
-    }
+    /**
+     * 获得前端传过来的文本数据
+     * @param text 文书内容
+     */
+    @RequestMapping("/requestText")
+    public void getMessage(@RequestParam(value = "text", defaultValue = "我是一名大学生") String text) {
 
-    public void init() {
-        pipeline = new StanfordCoreNLP("StanfordCoreNLP-chinese.properties");
-    }
-
-    private String segInCh(String text) {
-        // 载入properties文件
-        Annotation annotation = new Annotation(text);
-
-        // 解析
-        pipeline.annotate(annotation);
-        // pipeline.prettyPrint(annotation, System.out);
-        List<CoreMap> sentences = annotation.get(CoreAnnotations.SentencesAnnotation.class);
-
-        List<String> res = new ArrayList<>();
-        for (CoreMap sentence : sentences) {
-            for (CoreLabel token : sentence.get(CoreAnnotations.TokensAnnotation.class)) {
-                String word = token.get(CoreAnnotations.TextAnnotation.class);
-                res.add(word);
-            }
-        }
-
-        output(sentences);
-
-        StringBuilder tokens = new StringBuilder();
-        for (String word : res) {
-            tokens.append(word);
-            tokens.append("\n");
-        }
-
-        return tokens.toString();
-    }
-
-    private void output(List<CoreMap> sentences) {
-        for (CoreMap sentence : sentences) {
-            for (CoreLabel token : sentence.get(CoreAnnotations.TokensAnnotation.class)) {
-                // 获取分词
-                String word = token.get(CoreAnnotations.TextAnnotation.class);
-                // 获取词性标注
-                String pos = token.get(CoreAnnotations.PartOfSpeechAnnotation.class);
-                // 获取命名实体识别结果
-                String ner = token.get(CoreAnnotations.NamedEntityTagAnnotation.class);
-                // 获取词形还原结果
-                String lemma = token.get(CoreAnnotations.LemmaAnnotation.class);
-                System.out.println(word + "\t" + pos + "\t" + ner + "\t" + lemma);
-            }
-
-            Tree tree = sentence.get(TreeCoreAnnotations.TreeAnnotation.class);
-            System.out.println(tree.toString());
-        }
-    }
-
-    public static void main(String[] args) throws Exception {
-        Controller controller = new Controller();
-        String res = controller.getStr("我是一名大学生，现在在南京读书，我的专业是软件工程");
-        System.out.println(res);
     }
 }
---
+++ court/src/main/java/com/nju/edu/court/entity/Analysis.java
@@ -0,0 +1,68 @@
+package com.nju.edu.court.entity;
+
+import com.hankcs.hanlp.HanLP;
+import com.hankcs.hanlp.seg.common.Term;
+import com.hankcs.hanlp.tokenizer.NLPTokenizer;
+import com.hankcs.hanlp.tokenizer.StandardTokenizer;
+import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 分析类
+ * @author Zyi
+ */
+@Component
+public class Analysis {
+
+    private String paragraph;
+    private Map<String, List<String>> res;
+    private StanfordCoreNLP pipeline;
+
+    public Analysis(String paragraph) {
+        this.paragraph = paragraph;
+        this.res = new HashMap<>();
+        System.out.println("the map is init!");
+        init();
+    }
+
+    public Analysis() {
+        this.res = new HashMap<>();
+        System.out.println("the map is init!");
+    }
+
+    private void init() {
+        // 加载默认的配置文件
+        // 后续可以根据自己的需求进行修改
+        this.pipeline = new StanfordCoreNLP("StanfordCoreNLP-chinese.properties");
+    }
+
+    /**
+     * 获得需要分析的内容
+     * @return 文书内容
+     */
+    public String getParagraph() {
+        return paragraph;
+    }
+
+    public void setParagraph(String paragraph) {
+        this.paragraph = paragraph;
+    }
+
+    /**
+     * 获得分析完成的结果
+     * @return 完成词性标注后的结果
+     */
+    public Map<String, List<String>> getRes() {
+        return res;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(HanLP.segment("你好，欢迎使用HanLP汉语处理包！"));
+        List<Term> termList = StandardTokenizer.segment("商品和服务");
+        System.out.println(termList);
+    }
+}
